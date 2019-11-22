579d778aa966ab1c94c23f3104d17a85293a78c7
Nattakorn <perth.ploy@gmail.com>
2019-11-22 09:33:48 +0700

    Finish DisplayPart class

--- src/gui/DisplayPart.java
+++ src/gui/DisplayPart.java
@@ -1,17 +1,16 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 
-public class DisplayPart{
+public class DisplayPart extends Label{
 	
 	public DisplayPart() {
-		
-		// implement your code here
-		
+		setPadding(new Insets(10));
+		setStyle("-fx-font-size : 18;");
 	}
 	
 	public void update(String text) {
-
-		// implement your code here
-		
+		setText(text);
 	}
 }
