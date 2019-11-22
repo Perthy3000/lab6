54434b8d8333766f85b58781eefa5719d6435afc
Nattakorn <perth.ploy@gmail.com>
2019-11-22 09:46:00 +0700

    Finish TimerGUI class

--- src/gui/TimerGUI.java
+++ src/gui/TimerGUI.java
@@ -1,26 +1,37 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
-public class TimerGUI{
+public class TimerGUI extends VBox{
 
-	// implement your code here
-	
+	private Label nameLabel;
+	private DisplayPart displayPart;
+	private ControlPart controlPart;
 
 	public TimerGUI(String name) {
-		
-
-		// implement your code here
-		
+		setAlignment(Pos.CENTER);
+		setPadding(new Insets(10));
+		setSpacing(10);
+		nameLabel = new Label(name);
+		nameLabel.setStyle("-fx-font-size : 22;");
+		displayPart = new DisplayPart();
+		controlPart = new ControlPart();
+		getChildren().addAll(nameLabel, displayPart, controlPart);
 	}
 	
 	public String getName() {
-
-		// implement your code here
-		
+		return nameLabel.getText().trim();
 	}
-	
-	
-	// Generate getters 
-	
+
+	public DisplayPart getDisplayPart() {
+		return displayPart;
+	}
+
+	public ControlPart getControlPart() {
+		return controlPart;
+	}
 	
 }
