f5c8a35885270abeab79001f33d7b6bf4115d5e0
Nattakorn <perth.ploy@gmail.com>
2019-11-22 11:44:23 +0700

    Finish Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,26 @@ package main;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import logic.Timer;
 
 public class Main extends Application {
-	// implement your code here
+	
+	public static Timer timer;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		// implement your code here
+		HBox rootBox = new HBox();
+		Timer countdownTimer = new Timer("countdown", -1);
+		timer = countdownTimer;
+		Timer countupTimer1 = new Timer("countup1", 1);
+		Timer countupTimer2 = new Timer("countup2", 1);
+		rootBox.getChildren().addAll(countdownTimer.getTimerGUI(), countupTimer1.getTimerGUI(), countupTimer2.getTimerGUI());
+		Scene scene1 = new Scene(rootBox, 500, 500);
+		primaryStage.setScene(scene1);
+		primaryStage.show();
 	}
 	
 	public static void main (String [] args) {
